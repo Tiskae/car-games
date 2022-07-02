c52a1e1a66ca8b20f68c273d806a3ab3a332856d
user <user@users-MacBook-Pro.local>
2022-07-02 23:49:19 +0100

    Added Categoroes game boxes

--- assets/colors.ts
+++ assets/colors.ts
@@ -1,3 +1,13 @@
 export default {
   primary: "#a5d8ff",
+  // primary: "#fff",
+  cyanLight: "#e3fafc",
+  tealLight: "#e6fcf5",
+  greenLight: "#d3f9d8",
+  yellowLight: "#fff9db",
+  orangeLight: "#fff4e6",
+  pinkLight: "#fff0f6",
+  redLight: "#ffe3e3",
+  greyLight: "#e9ecef",
+  violetLight: "#e5dbff",
 };
--- screens/GameCategories.tsx
+++ screens/GameCategories.tsx
@@ -1,11 +1,47 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import colors from "../assets/colors";
+import GameBox from "../components/GameBox";
+import {
+  MaterialIcons,
+  Ionicons,
+  FontAwesome,
+  Fontisto,
+} from "@expo/vector-icons";
 
 const GameCategories = () => {
   return (
-    <View style={styles.container}>
-      <Text>Game categories component</Text>
-    </View>
+    <ScrollView style={styles.container} scrollEnabled={true}>
+      <GameBox
+        title="Price quiz"
+        backgroundColor={colors.greenLight}
+        icon={<MaterialIcons name="attach-money" size={50} color="black" />}
+      />
+      <GameBox title="True or false" backgroundColor={colors.cyanLight} />
+      <GameBox
+        title="Logo quiz"
+        backgroundColor={colors.greyLight}
+        icon={<Fontisto name="react" size={50} color="black" />}
+      />
+      <GameBox
+        title="Guess the car"
+        backgroundColor={colors.orangeLight}
+        icon={<Ionicons name="car-sport-outline" size={50} color="black" />}
+        locked
+      />
+      <GameBox
+        title="Power quiz"
+        backgroundColor={colors.redLight}
+        icon={<FontAwesome name="superpowers" size={50} color="black" />}
+        locked
+      />
+      <GameBox
+        title="Speed quiz"
+        backgroundColor={colors.violetLight}
+        icon={<MaterialIcons name="speed" size={50} color="black" />}
+        locked
+      />
+    </ScrollView>
   );
 };
 
@@ -13,6 +49,8 @@ export default GameCategories;
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
+    flexDirection: "column",
+    padding: 20,
+    flex: 1,
   },
 });
